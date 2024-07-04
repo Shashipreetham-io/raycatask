8b4a6adb8903688b1ee8fbcbeceeb7ef9a8b94a2
palavajalashashipreetham <61183907+palavajalashashipreetham@users.noreply.github.com>
2024-07-04 19:58:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import Image from "next/image";
 import { useRouter } from 'next/router';
+import Page from "./Main/page";
 
 export default function Home() {
 
   
   return (
     <main >
-                     
-             
+          <Page/>   
     </main>
   );
 }

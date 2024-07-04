ba105464c8a497c99e95e7e6a21f603c09778ffb
palavajalashashipreetham <61183907+palavajalashashipreetham@users.noreply.github.com>
2024-07-04 19:38:33 -0400

    Issues resolved
    Issues resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
 import { useRouter } from 'next/router';
-import Page from "./sidebar/page";
 
 export default function Home() {
 
@@ -8,7 +7,7 @@ export default function Home() {
   return (
     <main >
                      
-             <Page/>
+             
     </main>
   );
 }
